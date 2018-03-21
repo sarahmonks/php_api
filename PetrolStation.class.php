767c69bc20c857d32c49918cb058f3b4cc64ccf4
sarahmonks <sarah.monks@webelevate.ie>
2018-03-21 18:42:20 +0000

    Tidied formatting

--- PetrolStation.class.php
+++ PetrolStation.class.php
@@ -4,9 +4,9 @@
  */
 class PetrolStation{
 
-	private $DatabaseConnection;
-	private $ErrorHandler;
-	private $Review; //to store an instance of the Review class
+    private $DatabaseConnection;
+    private $ErrorHandler;
+    private $Review; //to store an instance of the Review class
     private $stationID = null;    //initialize the stationID to null
    	private $stationName; 
     private $stationAddressLine1;    
@@ -19,49 +19,47 @@ class PetrolStation{
     private $reviews = array();  
 
 
-	public function __construct(){
-		$this->DatabaseConnection = DatabaseConnection::getInstance();
-		$this->ErrorHandler = new ErrorHandler("PetrolStation");
-		$this->Review = new Review();
-	}
+    public function __construct(){
+        $this->DatabaseConnection = DatabaseConnection::getInstance();
+        $this->ErrorHandler = new ErrorHandler("PetrolStation");
+        $this->Review = new Review();
+    }
 
     public function getAllStationsMapData(){
         /*
-		 */
-		$allStationsPositionData = array();
+         */
+        $allStationsPositionData = array();
         foreach($this->DatabaseConnection->getAllStationsMapData() as $key => $value){	
             array_push(
-            	$allStationsPositionData, array(
-            		'stationID' => $value['stationID'],
-            		'stationName' => $value['stationName'],
-            		'stationAddressLine2' => $value['stationAddressLine2'],
-		            'stationLatLng' => array("lat" => $value['stationLatitude'], "lng" => $value['stationLongitude'])
-		            )
-            	); 
+                $allStationsPositionData, array(
+                    'stationID' => $value['stationID'],
+                    'stationName' => $value['stationName'],
+                    'stationAddressLine2' => $value['stationAddressLine2'],
+                    'stationLatLng' => array("lat" => $value['stationLatitude'], "lng" => $value['stationLongitude'])
+                    )
+                ); 
         }
-		return $allStationsPositionData;
+        return $allStationsPositionData;
     }
 
 
-	public function getStationDetails($stationID){
-		/*
-		 * This method takes in a stationID and gets all station data related to that stationID.
-		 */
-		$this->stationID = $stationID;
+    public function getStationDetails($stationID){
+        /*
+         * This method takes in a stationID and gets all station data related to that stationID.
+         */
+        $this->stationID = $stationID;
         $stationData = array(); //initialize the $stationData array which will store the data we return from this method.
 		
-		//get the station data from the database. 
-		//An exception will be thrown in the DatabaseConnection class if the stationID does not exist in the database	
-		$stationData = $this->DatabaseConnection->getStationData($this->stationID);	 
+        //get the station data from the database. 
+        //An exception will be thrown in the DatabaseConnection class if the stationID does not exist in the database	
+        $stationData = $this->DatabaseConnection->getStationData($this->stationID);	 
 
-		//get the review data for this stationID and store it in a key called 'reviews' in the stationData array
-	    $stationData['reviews'] = $this->Review->getReviews($this->stationID);
-		//get the stationServices data for this stationID and store it in a key called 'stationServices' in the stationData array
-		$stationData['stationServices'] = $this->DatabaseConnection->getStationServicesData($this->stationID);
-
-		return $stationData;
-		
-	}
+        //get the review data for this stationID and store it in a key called 'reviews' in the stationData array
+        $stationData['reviews'] = $this->Review->getReviews($this->stationID);
+        //get the stationServices data for this stationID and store it in a key called 'stationServices' in the stationData array
+        $stationData['stationServices'] = $this->DatabaseConnection->getStationServicesData($this->stationID);
+        return $stationData;	
+    }
 
 }
 
